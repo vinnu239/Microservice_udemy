a800e935b987ef9eb25bb1d4a9608587e96b2725
vinnu239 <vinod.shanmukha000@gmail.com>
2024-10-07 18:33:58 +0530

    Initial push of Student service

---
+++ student-service/src/main/java/com/infybuzz/app/StudentServiceApplication.java
@@ -0,0 +1,38 @@
+package com.infybuzz.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+@ComponentScan({"com.infybuzz.controller", "com.infybuzz.service"})
+@EntityScan("com.infybuzz.entity")
+@EnableJpaRepositories("com.infybuzz.repository")
+@EnableFeignClients("com.infybuzz.feignclients")
+@EnableEurekaClient
+public class StudentServiceApplication {
+	
+	@Value("${address.service.url}")
+	private String addressServiceUrl;
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApplication.class, args);
+		System.out.println("Student service up and running");
+	}
+	
+	@Bean
+	public WebClient webClient () {
+		WebClient webClient =WebClient.builder()
+				.baseUrl(addressServiceUrl).build();
+		
+		return webClient;
+	}
+
+}
---
+++ student-service/src/main/java/com/infybuzz/service/StudentService.java
@@ -0,0 +1,65 @@
+package com.infybuzz.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.infybuzz.entity.Student;
+import com.infybuzz.feignclients.AddressFeignClient;
+import com.infybuzz.repository.StudentRepository;
+import com.infybuzz.request.CreateStudentRequest;
+import com.infybuzz.response.AddressResponse;
+import com.infybuzz.response.StudentResponse;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@Autowired
+	WebClient webClient;
+	
+	@Autowired
+	AddressFeignClient addressFeignClient;
+
+	public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
+
+		Student student = new Student();
+		student.setFirstName(createStudentRequest.getFirstName());
+		student.setLastName(createStudentRequest.getLastName());
+		student.setEmail(createStudentRequest.getEmail());
+		
+		student.setAddressId(createStudentRequest.getAddressId());
+		student = studentRepository.save(student);
+		
+		StudentResponse studentResponse = new StudentResponse(student);
+		
+		//studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+		
+		studentResponse.setAddressResponse(addressFeignClient.getById(student.getAddressId()));
+
+		return studentResponse;
+	}
+	
+	public StudentResponse getById (long id) {
+		Student student = studentRepository.findById(id).get();
+		StudentResponse studentResponse = new StudentResponse(student);
+		
+		//studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+		
+		studentResponse.setAddressResponse(addressFeignClient.getById(student.getAddressId()));
+		
+		return studentResponse;
+	}
+	
+	public AddressResponse getAddressById (long addressId) {
+		Mono<AddressResponse> addressResponse = 
+				webClient.get().uri("/getById/" + addressId)
+		.retrieve().bodyToMono(AddressResponse.class);
+		
+		return addressResponse.block();
+	}
+}
