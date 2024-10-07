b3fc93e553a3a6912ca82af74f19fb2435ce6b3f
vinnu239 <vinod.shanmukha000@gmail.com>
2024-10-07 21:27:01 +0530

    pushing api-gateway changes

--- student-service/src/main/java/com/infybuzz/feignclients/AddressFeignClient.java
+++ student-service/src/main/java/com/infybuzz/feignclients/AddressFeignClient.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.infybuzz.response.AddressResponse;
 
-@FeignClient(value = "address-service",
-		path = "/api/address")
+//@FeignClient(value = "address-service",
+//		path = "/api/address")
+//public interface AddressFeignClient {
+//
+//	@GetMapping("/getById/{id}")
+//	public AddressResponse getById(@PathVariable long id);
+
+// the above code is using feign client with eureka server we are calling address method 
+// but to use this class for all the micro service methos commonly and also we need to call the address method
+// thorugh api gateway follow the below code
+
+@FeignClient(value = "api-gateway")
 public interface AddressFeignClient {
 
-	@GetMapping("/getById/{id}")
+	@GetMapping("/address-service/api/address/getById/{id}")
 	public AddressResponse getById(@PathVariable long id);
+	
+	
 	
 }
--- student-service/src/main/java/com/infybuzz/feignclients/AdrSerLoadBalConfig.java
+++ student-service/src/main/java/com/infybuzz/feignclients/AdrSerLoadBalConfig.java
@@ -1,18 +1,21 @@
-package com.infybuzz.feignclients;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.context.annotation.Bean;
 
-import feign.Feign;
-
-@LoadBalancerClient(value = "address-service")
-public class AdrSerLoadBalConfig {
-
-	@LoadBalanced
-	@Bean
-	public Feign.Builder feignBuilder () {
-		return Feign.builder();
-	}
-	
-}
+// if we are using api gateway than it will provides the load balancing as well so the below code wont be needed
+//package com.infybuzz.feignclients;
+//
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+//import org.springframework.context.annotation.Bean;
+//
+//import feign.Feign;
+//
+//@LoadBalancerClient(value = "address-service")
+//public class AdrSerLoadBalConfig {
+//
+//	@LoadBalanced
+//	@Bean
+//	public Feign.Builder feignBuilder () {
+//		return Feign.builder();
+//	}
+//	
+//}
