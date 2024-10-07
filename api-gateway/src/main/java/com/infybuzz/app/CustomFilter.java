2eeaec197f5bc7c18c604803ab20b2a135650ec2
vinnu239 <vinod.shanmukha000@gmail.com>
2024-10-07 21:27:59 +0530

    gate-way changes

---
+++ api-gateway/src/main/java/com/infybuzz/app/CustomFilter.java
@@ -0,0 +1,37 @@
+package com.infybuzz.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class CustomFilter implements GlobalFilter {
+
+	Logger logger = LoggerFactory.getLogger(CustomFilter.class);
+
+	// prefilter 
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		ServerHttpRequest request = exchange.getRequest();
+		//if we need to execute post filter only to specific micro sevice use as below
+		
+		// if(request.getURI().tostring().contains("spcific url path like /api/student/")
+		
+		logger.info("Authoriation = " + request.getHeaders().getFirst("Authorization"));
+		
+		// post filter 
+		return chain.filter(exchange).then(Mono.fromRunnable(() ->{
+			ServerHttpResponse response = exchange.getResponse();
+			logger.info("postfilter="+ response.getStatusCode());			
+		}));
+	}
+	
+}
