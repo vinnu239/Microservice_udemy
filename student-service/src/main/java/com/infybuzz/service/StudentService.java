32ed5d93aaf583cf3ae76498b0911d81a0011bb1
vinnu239 <vinod.shanmukha000@gmail.com>
2024-10-08 21:47:08 +0530

    Sleuth and zipkin changes

--- student-service/src/main/java/com/infybuzz/service/StudentService.java
+++ student-service/src/main/java/com/infybuzz/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.infybuzz.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,6 +18,7 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 @Service
 public class StudentService {
 
+	Logger logger = LoggerFactory.getLogger(StudentService.class);
 	@Autowired
 	StudentRepository studentRepository;
 	
@@ -48,6 +51,7 @@ public class StudentService {
 	}
 	
 	public StudentResponse getById (long id) {
+		logger.info("Inside student geyByid");
 		Student student = studentRepository.findById(id).get();
 		StudentResponse studentResponse = new StudentResponse(student);
 		
